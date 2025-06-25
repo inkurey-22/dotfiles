d15b3cbcfba6de973aac4a60e4cd158f1b8a104f
Théophile "Inkurey22" R. <inkurey22.tr@proton.me>
2025-06-25 15:35:52 +0200

    FIX: update link

--- link.sh
+++ link.sh
@@ -40,17 +40,17 @@ chsh -s $(which zsh)
 
 # === LINK DOTFILES === #
 
-ln -sf "$HOME/.zshrc" "$DOTFILES_DIR/shell/zshrc"
-ln -sf "$HOME/.zshenv" "$DOTFILES_DIR/shell/zshenv"
-ln -sf "$HOME/.p10k.graphical.zsh" "$DOTFILES_DIR/shell/p10k.graphical.zsh"
-ln -sf "$HOME/.p10k.tty.zsh" "$DOTFILES_DIR/shell/p10k.tty.zsh"
+ln -sf "$DOTFILES_DIR/shell/zshrc" "$HOME/.zshrc"
+ln -sf "$DOTFILES_DIR/shell/zshenv" "$HOME/.zshenv"
+ln -sf "$DOTFILES_DIR/shell/p10k.graphical.zsh" "$HOME/.p10k.graphical.zsh"
+ln -sf "$DOTFILES_DIR/shell/p10k.tty.zsh" "$HOME/.p10k.tty.zsh"
 
-ln -sf "$CONFIG_DIR/nvim" "$DOTFILES_DIR/config/nvim"
-ln -sf "$CONFIG_DIR/kitty" "$DOTFILES_DIR/config/kitty"
-ln -sf "$CONFIG_DIR/sway" "$DOTFILES_DIR/config/sway"
-ln -sf "$CONFIG_DIR/waybar" "$DOTFILES_DIR/config/waybar"
-ln -sf "$CONFIG_DIR/fastfetch" "$DOTFILES_DIR/config/fastfetch"
-ln -sf "$CONFIG_DIR/Code" "$DOTFILES_DIR/config/Code"
-ln -sf "$CONFIG_DIR/rofi" "$DOTFILES_DIR/config/rofi"
+ln -sf "$DOTFILES_DIR/config/nvim" "$CONFIG_DIR/nvim"
+ln -sf "$DOTFILES_DIR/config/kitty" "$CONFIG_DIR/kitty"
+ln -sf "$DOTFILES_DIR/config/sway" "$CONFIG_DIR/sway"
+ln -sf "$DOTFILES_DIR/config/waybar" "$CONFIG_DIR/waybar"
+ln -sf "$DOTFILES_DIR/config/fastfetch" "$CONFIG_DIR/fastfetch"
+ln -sf "$DOTFILES_DIR/config/Code" "$CONFIG_DIR/Code"
+ln -sf "$DOTFILES_DIR/config/rofi" "$CONFIG_DIR/rofi"
 
 konsave -i "$DOTFILES_DIR/curry-catppuccin.knsv"
