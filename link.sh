54892883b0ad6992e4141a800634b43da6c2f2d6
Théophile "Inkurey22" R. <inkurey22.tr@proton.me>
2025-08-09 11:03:08 +0200

    FIX: now just copies dotfiles. No install or anything because... meh, i'm lazy

--- link.sh
+++ link.sh
@@ -3,37 +3,7 @@
 CONFIG_DIR="$HOME/.config"
 DOTFILES_DIR="$HOME/dotfiles"
 
-# === INSTALL REQUIREMENTS BASED ON OS === #
-if [ -f /etc/os-release ]; then
-    . /etc/os-release
-    if [[ "$ID" == "arch" || "$ID_LIKE" == *"arch"* ]]; then
-        yay -S --needed --noconfirm \
-            zsh \
-            kitty \
-            neovim \
-            fastfetch \
-    elif [[ "$ID" == "fedora" || "$ID_LIKE" == *"fedora"* ]]; then
-        sudo dnf install -y \
-            zsh \
-            kitty \
-            neovim \
-            fastfetch
-    else
-        echo "Unsupported OS: $ID"
-        exit 1
-    fi
-else
-    echo "/etc/os-release not found. Cannot detect OS."
-    exit 1
-fi
-
-# === SET ZSH AS DEFAULT SHELL === #
-chsh -s $(which zsh)
-
-# === LINK DOTFILES === #
-
 cp -f "$DOTFILES_DIR/shell/zshrc" "$HOME/.zshrc"
-cp -f "$DOTFILES_DIR/shell/zshenv" "$HOME/.zshenv"
 cp -f "$DOTFILES_DIR/shell/p10k.graphical.zsh" "$HOME/.p10k.graphical.zsh"
 cp -f "$DOTFILES_DIR/shell/p10k.tty.zsh" "$HOME/.p10k.tty.zsh"
 
