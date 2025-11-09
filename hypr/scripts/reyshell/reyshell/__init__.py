c7af6d5d9f615a8fb4fd827b591c1fa8768a3ebb
Théophile R. <146175466+inkurey-22@users.noreply.github.com>
2025-11-09 10:40:08 +0100

    removed hyprland and quickshell

--- hypr/scripts/reyshell/reyshell/__init__.py
+++
@@ -1,9 +0,0 @@
-from reyshell.parser import parse_args
-
-
-def main() -> None:
-    parser, args = parse_args()
-    if "cls" in args:
-        args.cls(args).run()
-    else:
-        parser.print_help()
