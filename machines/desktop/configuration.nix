ecac7f32c403ed0196989e50881bc6e2d040dd02
Théophile "Inkurey22" R. <inkurey22.tr@proton.me>
2026-04-06 15:58:58 +0200

    removed a curly bracket that was too much in desktop config

--- machines/desktop/configuration.nix
+++ machines/desktop/configuration.nix
@@ -7,4 +7,3 @@
   # Desktop-specific LUKS configuration (for swap)
   boot.initrd.luks.devices."luks-5ce730a9-d295-4e17-9a0a-b442bc5e6c32".device = "/dev/disk/by-uuid/5ce730a9-d295-4e17-9a0a-b442bc5e6c32";
 }
-}
