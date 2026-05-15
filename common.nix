fccc2576f43e00d0fc2493dbbaf413b7ebe1f94f
Théophile "Inkurey22" R. <inkurey22.tr@proton.me>
2026-05-15 10:07:37 +0200

    added tailscale

--- common.nix
+++ common.nix
@@ -165,7 +165,7 @@
     pinentryPackage = pkgs.pinentry-all;
     enableSSHSupport = true;
   };
-
+  services.tailscale.enable = true;
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
