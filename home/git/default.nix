90a394675f8e731c45a6c7ac39a26a631df4c485
Théophile R. <inkurey22.tr@proton.me>
2026-05-25 13:07:42 +0200

    git config

--- home/git/default.nix
+++ home/git/default.nix
@@ -1,7 +1,18 @@
-{ ... }:
-{
-  home.file.git_config = {
-    source = ./.gitconfig;
-    target = ".gitconfig";
+{ ... }: {
+  programs.git = {
+    enable = true;
+    lfs.enable = true;
+    settings = {
+      user = {
+        name = "Théophile R.";
+        email = "inkurey22.tr@proton.me";
+      };
+      init.defaultBranch = "main";
+      commit.verbose = "yes";
+    };
+    extraConfig = {
+      user.signingKey = "8B40CD831C17DFBE8EC74FCDFDFB4C182419731B!";
+      commit.gpgSign = true;
+    };
   };
 }
